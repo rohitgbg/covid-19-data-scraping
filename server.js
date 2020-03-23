6dd31faa5de4cab734e11d1748a0fd09a8b6399f
Rohith GB <rohit.gb@preceptlabs.com>
2020-03-24 00:09:33 +0530

    scraped indian covid-19 data

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotevn = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const cheerio = require("cheerio");
+const request = require("request");
 
 dotevn.config({ path: "./config/config.env" });
 
@@ -16,6 +18,53 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Mount routes
+app.get("/scrape", (req, res) => {
+  // The URL we will scrape from - in our example Anchorman 2.
+  url = "https://www.mohfw.gov.in/";
+
+  const indianData = new Set();
+
+  request(url, function(error, response, html) {
+    if (!error) {
+      const scrapedData = [];
+
+      const $ = cheerio.load(html);
+      $(".newtab   .table-dark  tbody > tr ").each((index, element) => {
+        const tds = $(element).find("td");
+
+        const id = $(tds[0]).text();
+        const state = $(tds[1]).text();
+        const totalConfIndian = $(tds[2]).text();
+        const totalConfForeign = $(tds[3]).text();
+        const cured = $(tds[4]).text();
+        const death = $(tds[5]).text();
+
+        let data;
+        if (id.length < 4) {
+          data = {
+            id,
+            state,
+            totalConfIndian,
+            totalConfForeign,
+            cured,
+            death
+          };
+        }
+
+        scrapedData.push(data);
+      });
+
+      console.log(scrapedData);
+    }
+  });
+
+  res.status(200).json({
+    success: true,
+    data: "Hello"
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
